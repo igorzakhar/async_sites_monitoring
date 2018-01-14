86e5b4758a68a0c11b12351ed68f0b7fc30afe9f
igorzakhar <igorzakhar@gmail.com>
2018-01-14 16:42:05 +0300

    Rename some variables

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,4 +1,5 @@
 import asyncio
+import argparse
 from datetime import datetime, timezone
 import re
 from urllib.parse import urlparse
@@ -52,7 +53,8 @@ async def check_sites_health(path_to_urls, loop):
 
 
 def parse_expiry_date(domain, whois_response):
-    domain_chunks = domain.split('.', 2)
+    maxsplit = 2
+    domain_chunks = domain.split('.', maxsplit)
     top_level_domain = domain_chunks[-1]
     pattern = getattr(regex_map, top_level_domain)
     expiration_date = re.search(
@@ -64,26 +66,31 @@ def parse_expiry_date(domain, whois_response):
     return dateutil.parser.parse(expiration_date)
 
 
-def print_results(results):
+def print_results(check_sites_results):
     date_now = datetime.now(timezone.utc)
     status_string = '{:20s}: status code: {}, days until expiry: {} days ({})'
-    for result in results:
+    for check_site in check_sites_results:
         expiry_date = parse_expiry_date(
-            result['domain'],
-            result['whois_info']
+            check_site['domain'],
+            check_site['whois_info']
         )
         days_until_expiry = expiry_date - date_now
         print(status_string.format(
-            result['domain'],
-            result['http_status'],
+            check_site['domain'],
+            check_site['http_status'],
             days_until_expiry.days,
             expiry_date.date()
         ))
 
 
 if __name__ == '__main__':
-    path = 'urls.txt'
-    loop = asyncio.get_event_loop()
-    results = loop.run_until_complete(check_sites_health(path, loop))
-    print_results(results)
-    loop.close()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", type=str, help="Path to file")
+    args = parser.parse_args()
+    if args.file:
+        loop = asyncio.get_event_loop()
+        results = loop.run_until_complete(check_sites_health(args.file, loop))
+        print_results(results)
+        loop.close()
+    else:
+        print("You need to specify a file path with urls list")
